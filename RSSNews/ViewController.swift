567d672b4d2128580e426532edee12b66fa1de7f
Denis Kovalev <denis.kovalev@saritasa.com>
2019-02-09 16:49:16 +0700

    Initial Commit

---
+++ RSSNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSNews
+//
+//  Created by User on 09/02/2019.
+//  Copyright © 2019 Saritasa Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
