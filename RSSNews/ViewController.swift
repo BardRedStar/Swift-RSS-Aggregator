d0289bcda5c92d4b9c6e00fb3838c7cac1204547
Denis Kovalev <denis.kovalev@saritasa.com>
2019-02-09 17:28:06 +0700

    Add table for showing news
    - Added doc for custom methods
    - Added plist resource file for loading news
    
    #MISC-???

--- RSSNews/ViewController.swift
+++ RSSNews/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
+/// A main class of application
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var newsTableView: UITableView!
+    
+    var newsDictionary: NSDictionary?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        readPropertyList()
+        
+        newsTableView.dataSource = self
     }
 
-
+    /// Reads the news from the properties file (.pfile) and put it into global dictionary
+    func readPropertyList() {
+        
+        if let path = Bundle.main.path(forResource: "news", ofType: "plist") {
+            newsDictionary = NSDictionary(contentsOfFile: path)
+        }
+        else {
+            fatalError("File is not found!")
+        }
+    }
 }
 
+/// An extension for viewcontroller class to use table view
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return newsDictionary!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RSSUITableViewCell", for: indexPath) as? RSSTableViewCell
+            else {
+            fatalError("This isn't a RSSUITableViewCell object!")
+        }
+        
+        // Get news by index from dictionary (sorry for that)
+        var i = 0
+        for (title, content) in newsDictionary! {
+            if (i == indexPath.row) {
+                cell.titleLabel.text = (title as? String) ?? "Unknown"
+                cell.contentLabel.text = (content as? String) ?? "Unknown"
+            }
+            i += 1
+        }
+        
+        return cell
+    }
+    
+    
+}
