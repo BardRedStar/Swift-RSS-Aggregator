03ce97d9cb6e13d7daee478ff2aae55d16eb8503
Denis Kovalev <denis.kovalev@saritasa.com>
2019-02-21 15:20:29 +0700

    Add data class for news content
    #MISC-???

---
+++ RSSNews/Model/NewsItem.swift
@@ -0,0 +1,24 @@
+//
+//  NewsItem.swift
+//  RSSNews
+//
+//  Created by User on 21/02/2019.
+//  Copyright © 2019 Saritasa Inc. All rights reserved.
+//
+
+import Foundation
+
+
+/// A data-class for holding the news item information
+open class NewsItem: Codable {
+    var title: String
+    var content: String
+    
+    /// - Parameters:
+    ///   - title: News item title
+    ///   - content: News item content
+    init(title: String, content: String){
+        self.title = title
+        self.content = content
+    }
+}
