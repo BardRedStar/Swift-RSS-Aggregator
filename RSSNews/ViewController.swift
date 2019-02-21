0754920357131106901d0a4a979cb3021e3972ce
Denis Kovalev <denis.kovalev@saritasa.com>
2019-02-21 15:18:36 +0700

    Add search bar to filter news by content
    #MISC-???

--- RSSNews/ViewController.swift
+++ RSSNews/ViewController.swift
@@ -11,26 +11,51 @@ import UIKit
 /// A main class of application
 class ViewController: UIViewController {
     
-    @IBOutlet weak var newsTableView: UITableView!
+    @IBOutlet private weak var newsTableView: UITableView!
     
-    private var newsDictionary: NSDictionary?
+    private var newsSearchController: UISearchController!
+    
+    private var newsArray: [NewsItem]!
+    private var filteredNewsArray: [NewsItem]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        readPropertyList ()
+        // Geting data
+        readPropertyList()
         
-        newsTableView.dataSource = self
+        // Setting up controls
+        setUpTableView()
+        setUpSearchBar()
     }
     
     /// Reads the news from the properties file (.pfile) and put it into global dictionary
-    func readPropertyList() {
+    func readPropertyList(){
         
-        if let path = Bundle.main.path(forResource: "news", ofType: "plist") {
-            newsDictionary = NSDictionary(contentsOfFile: path)
+        if let path = Bundle.main.path(forResource: "news", ofType: "plist"),
+            let xml = FileManager.default.contents(atPath: path){
+            
+            newsArray = try? PropertyListDecoder().decode([NewsItem].self, from: xml)
+            filteredNewsArray = [NewsItem]()
         } else {
             fatalError("File is not found!")
         }
+    }
+    
+    /// Sets up the table view
+    func setUpTableView(){
+        newsTableView.dataSource = self
+    }
+    
+    /// Sets up the search bar
+    func setUpSearchBar(){
+        newsSearchController = UISearchController(searchResultsController: nil)
+        
+        newsSearchController.searchResultsUpdater = self
+        newsSearchController.obscuresBackgroundDuringPresentation = false
+        newsSearchController.searchBar.placeholder = "Search news"
+        navigationItem.searchController = newsSearchController
+        definesPresentationContext = true
     }
 }
 
@@ -38,7 +63,7 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return newsDictionary!.count
+        return (isDataFiltering()) ? filteredNewsArray.count : newsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -48,16 +73,51 @@ extension ViewController: UITableViewDataSource {
                 fatalError("This isn't a RSSUITableViewCell object!")
         }
         
-        // Get news by index from dictionary (sorry for that)
-        var i = 0
-        for (title, content) in newsDictionary! {
-            if i == indexPath.row {
-                cell.title = (title as? String) ?? "Unknown"
-                cell.content = (content as? String) ?? "Unknown"
-            }
-            i += 1
-        }
+        let newsItem = (isDataFiltering()) ? filteredNewsArray[indexPath.row] : newsArray[indexPath.row]
+        
+        cell.title = newsItem.title
+        cell.content = newsItem.content
         
         return cell
+    }
+}
+
+/// An extension fore viewcontroller class to use search bar
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchText: searchController.searchBar.text!)
+    }
+    
+    
+    /// Filters news items by search query text
+    ///
+    /// - Parameters:
+    ///   - searchText: Text to searching by
+    ///   - scope: Scope name (soon)
+    func filterContentForSearchText(searchText: String, scope: String = "All") {
+        filteredNewsArray.removeAll()
+        filteredNewsArray = newsArray!.filter({(item: NewsItem) -> Bool in
+            return item.title.lowercased().contains(searchText.lowercased()) ||
+                item.content.lowercased().contains(searchText.lowercased())
+        })
+        
+        newsTableView.reloadData()
+    }
+    
+    
+    /// Checks searchbar emptiness
+    ///
+    /// - Returns: True if the searchbar is empty, false otherwise
+    func searchBarIsEmpty() -> Bool {
+        return newsSearchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    
+    /// Checks the filtering mode
+    ///
+    /// - Returns: True if the filter mode is active, false otherwise
+    func isDataFiltering() -> Bool {
+        return newsSearchController.isActive && !searchBarIsEmpty()
     }
 }
